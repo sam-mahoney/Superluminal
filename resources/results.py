a4ba50923d1030a43417be6e64338a94f33aebd8
Sam Mahoney <mahoney@cmui.co.uk>
2022-02-28 14:32:16 +0000

    - Implemented TaskHistory Logic (results.py)

--- resources/results.py
+++ resources/results.py
@@ -4,9 +4,10 @@ import json
 from flask import request, Response
 from flask_restful import Resource
 
-from database.models import Task, Result
+from database.models import Task, Result, TaskHistory
 from common.parse_utils import parse_request
 from common.types.task_types import Status
+
 
 class Results(Resource):
     """
@@ -17,6 +18,7 @@ class Results(Resource):
     get  : returns all results objects to user (-> CLI)
     post : adds result(s) to the database      (implant ->)
     """
+
     # List results
     def get(self):
         # Get all the results and return them to the user
@@ -60,16 +62,23 @@ class Results(Resource):
         if not json_obj['success']:
             json_head['task_status'] = Status.FAILED.name
         # check task_id's match and check task is executing
-        if json_head['task_id'] != json_obj['task_id']\
-                or json_head['task_status'] is not Status.EXECUTING.name:
+        if json_head['task_id'] != json_obj['task_id'] \
+                or json_head['task_status'] != Status.EXECUTING.name:
             error = {"status code": "400", "error": "invalid task"}
             return Response(json.dumps(error), status=400)
-        json_head['task_status'] = Status.EXECUTED.name
+        task_id = json_obj['task_id']
+        # 1. query documents in history collection by task_id
+        history_obj = TaskHistory.objects().get(task_id=task_id)
+        print(f"History_Obj = {history_obj['task_id']}")
+        # Update the corresponding history document
+        history_obj['task_status'] = Status.EXECUTED.name
+        history_obj['task_results'] = json_obj
+        history_obj.save()
         # Delete the Task
-        head.delete()
-        print("[!] Task deleted")
+        Task.objects().get(task_id=task_id).delete()
+        print(f"[!] Task [{task_id}] deleted")
         # Save the result to the database
         Result(**json_obj).save()
-        return Response(Result.objects.skip(Result. objects.count() - 1).to_json(),
+        return Response(Result.objects.skip(Result.objects.count() - 1).to_json(),
                         mimetype="application/json",
                         status=200)
